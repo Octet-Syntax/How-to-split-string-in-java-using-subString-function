24ab37666bfbe0102d15d64af9847e169a3294d8
Octet-Syntax <121711778+Octet-Syntax@users.noreply.github.com>
2023-01-21 21:56:49 +0530

    Add files via upload

---
+++ app.java
@@ -0,0 +1,26 @@
+package StringSpliter;
+
+
+public class app 
+{
+    public static void main(String[] args) 
+    {
+        String val = "25|65|98|54";
+        int i = 0;
+        int lasti = 0;
+
+        while (i < val.length())
+        {
+            if(val.charAt(i) == '|')
+            {
+                System.out.println(val.substring(lasti , i));
+                lasti = i+1;
+            }
+            if(i == val.length() - 1)
+            {
+                System.out.println(val.substring(lasti , i+1));
+            }
+            i++;
+        }
+    }
+}
